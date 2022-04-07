43e6bed06b4e9e22dd4bb4da41ffc4aad3d7719a
naveen741 <96760074+naveen741@users.noreply.github.com>
2022-04-07 18:14:25 +0530

    Update Game.java

--- src/main/java/cardgame/Game.java
+++ src/main/java/cardgame/Game.java
@@ -3,7 +3,7 @@ public class Game {
 	public static void main(String[] args) {		
 		GamePlay ob=new GamePlay();
 		ob.start();
-		System.err("================================================");
+		System.out.println("================================================");
 		System.out.println("Game Starts");
 		ob.play();
 	}
