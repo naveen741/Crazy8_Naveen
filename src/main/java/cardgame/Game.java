c723e9d6f2e56d75319e96ef5d278075565e6a8a
naveen741 <96760074+naveen741@users.noreply.github.com>
2022-04-08 11:22:21 +0530

    Update Game.java

--- src/main/java/cardgame/Game.java
+++ src/main/java/cardgame/Game.java
@@ -4,7 +4,7 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class Game {	
-	static Logger logger=Logger.getLogger(GamePlay.class.getName());
+	static Logger logger=Logger.getLogger(Game.class.getName());
 	public static void main(String[] args) {		
 		GamePlay ob=new GamePlay();
 		ob.start();
