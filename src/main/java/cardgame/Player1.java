1a0ebbf8ddfed11fa5b03da3891444962cae9b12
naveen741 <96760074+naveen741@users.noreply.github.com>
2022-04-08 16:01:52 +0530

    Update Player1.java

--- src/main/java/cardgame/Player1.java
+++ src/main/java/cardgame/Player1.java
@@ -1,57 +1,9 @@
 package cardgame;
-
-import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-public class Player1 implements PlayerStrategy{
-	int playerId;
-	List<Integer> opponentIds;
-	List<Card> myCards;
-	Card topPileCard;
-	Card.Suit changedSuit;
-	Card outCard=null;
+public class Player1 extends CommonStrategy implements PlayerStrategy{
 	Logger logger=Logger.getLogger(Player1.class.getName());
-	public void init(int playerId, List<Integer> opponentIds) {
-		this.playerId=playerId;
-		this.opponentIds=opponentIds;
-	}
-	public void receiveInitialCards(List<Card> cards) {
-		this.myCards=cards;
-	}
-	public boolean shouldDrawCard(Card topPileCard, Card.Suit changedSuit) {
-		this.topPileCard=topPileCard;
-		this.changedSuit=changedSuit;
-		for(int i=0;i<myCards.size();i++) {
-			if(myCards.get(i).getRank().equals(Card.Rank.EIGHT)) {
-				return false;
-			}
-		}
-		return check();
-		
-	}
-	public boolean check() {
-		if(changedSuit==null) {
-			for(int i=0;i<myCards.size();i++) {
-				if(myCards.get(i).getSuit().equals(topPileCard.getSuit()) || myCards.get(i).getRank().equals(topPileCard.getRank())) {
-					return false;
-				}
-			}
-		}
-		else {
-			for(int i=0;i<myCards.size();i++) {
-				if(myCards.get(i).getSuit().equals(changedSuit)) {
-					return false;
-				}
-			}
-			
-		}
-		return true;
-	}
-	public void receiveCard(Card drawnCard) {
-		logger.log(Level.INFO,"Player1 recieved : {0}",drawnCard.getRank()+" "+drawnCard.getSuit());
-		myCards.add(drawnCard);
-	}
 	public Card playCard() {
 		
 		for(int i=0;i<myCards.size();i++) {
@@ -59,8 +11,8 @@ public class Player1 implements PlayerStrategy{
 				outCard=myCards.get(i);
 				printPlayed(i);
 				myCards.remove(i);
+				i--;
 				return outCard;
-				break;
 			}
 		}
 		return play();
